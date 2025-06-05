fdf6d742334f3d4d5ba4944cb7be1fa6fb2fb4a3
xiangye72 <79642157+ch777777@users.noreply.github.com>
2025-06-05 19:51:24 +0800

    Update 1.8版本.user.js

--- 1.8版本.user.js
+++ 1.8版本.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         b站 | bilibili | 哔哩哔哩 | 一键三连健康探针（BiliHealth Scan）
 // @namespace    http://tampermonkey.net/
-// @version      1.8-dev
+// @version      1.8.1
 // @description  一键三连健康探针（BiliHealth Scan）显示b站 | bilibili | 哔哩哔哩 点赞率、投币率、收藏率及Steam综合评级
 // @license      MIT
 // @author       向也
